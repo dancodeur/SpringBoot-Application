b884e23bf29a51f7a07ed48b1d674a69339c766e
elengadan@gmail.com <elengadan@gmail.com>
2025-03-08 12:54:59 +0100

    initialisation du framework springboot

---
+++ src/test/java/api_rest/dancodeur/DancodeurApplicationTests.java
@@ -0,0 +1,13 @@
+package api_rest.dancodeur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DancodeurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
